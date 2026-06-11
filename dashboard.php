0cc22df5827b93c5649a4a4f6071aadfd6125ec2
Komang Kurniawan W <komang.kurniawan@outlook.com>
2026-06-12 04:49:15 +0800

    tes

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
+   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Project UAS</title>
   <!-- Tell the browser to be responsive to screen width -->
